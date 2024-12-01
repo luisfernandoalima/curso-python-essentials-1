df5f30cf1c3b805ea711ba226b1635db3eb49d28
Luis Lima <luisfernando.lima2005@gmail.com>
2024-12-01 20:50:10 -0300

    Finalização do módulo 3

---
+++ modulo 3/ex003.py
@@ -0,0 +1,18 @@
+# Cubo - uma matriz tridimensional (3x3x3)
+ 
+cube = [[[':(', 'x', 'x'],
+         [':)', 'x', 'x'],
+         [':(', 'x', 'x']],
+ 
+        [[':)', 'x', 'x'],
+         [':(', 'x', 'x'],
+         [':)', 'x', 'x']],
+ 
+        [[':(', 'x', 'x'],
+         [':)', 'x', 'x'],
+         [':)', 'x', 'x']]]
+ 
+print(cube)
+print(cube[0][0][0])  # outputs: ':('
+print(cube[2][2][0])  # outputs: ':)'
+ 
